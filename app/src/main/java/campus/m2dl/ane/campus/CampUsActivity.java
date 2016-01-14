c5ff250994995ffe578bb1de57d2f4191b8ea06e
jochy <alexandre.dubois-2@etu.univ-tours.fr>
2016-01-14 11:27:42 +0100

    POC GPS position displayed on the map

--- app/src/main/java/campus/m2dl/ane/campus/CampUsActivity.java
+++ app/src/main/java/campus/m2dl/ane/campus/CampUsActivity.java
@@ -1,17 +1,20 @@
 package campus.m2dl.ane.campus;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.location.Location;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -20,6 +23,8 @@ import android.widget.Toast;
 import campus.m2dl.ane.campus.listener.CampUsLocationListener;
 
 public class CampUsActivity extends AppCompatActivity {
+
+    public static final int POS_SIZE = 25;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +37,7 @@ public class CampUsActivity extends AppCompatActivity {
         LocationListener locationListener = new CampUsLocationListener(this);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
-        System.out.println("configured");
-
+        // updateLocation(43.56291, 1.46613);
         // (1242; 1390) -> (43,56291; 1,46613)
     }
 
@@ -45,30 +49,35 @@ public class CampUsActivity extends AppCompatActivity {
     }
 
     public void updateLocation(double lat, double lon) {
-        ImageView imageView = (ImageView) findViewById(R.id.map);
-        imageView.buildDrawingCache();
-        Bitmap bitmap = imageView.getDrawingCache().copy(imageView.getDrawingCache().getConfig(), true);
-        // 1242  |
-        // 43    | lat
-        float x = (float) (lat * 1242 / 43.56291);
-        float y = (float) (lon * 1390 / 1.46613);
+        // Load file resolution
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), R.drawable.map_50_bmp, options);
+        int width = options.outWidth;
+        int height = options.outHeight;
 
-        Toast.makeText(getBaseContext(),x + " - " + y,Toast.LENGTH_LONG).show();
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.map_50_bmp);
+
+        //Point refPix = new Point(1242* bitmap.getWidth() / width, 1390 * bitmap.getHeight() / height);
+        //Point refPix = new Point(904 * bitmap.getWidth() / width, 1013 * bitmap.getHeight() / height);
+        Point refPix = new Point(443 * bitmap.getWidth() / width, 507 * bitmap.getHeight() / height);
+
+        float x = (float) (lat * refPix.x / 43.56291);
+        float y = (float) (lon * refPix.y / 1.46613);
+
+        Toast.makeText(getBaseContext(), x + " - " + y, Toast.LENGTH_LONG).show();
 
         Bitmap bmOverlay = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
-        BitmapShader shader = new BitmapShader(bmOverlay,
-                BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
-
+        bmOverlay.setPixel((int) x, (int) y, Color.RED);
         Canvas canvas = new Canvas(bmOverlay);
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.RED);
-        paint.setStrokeWidth(10);
-        paint.setShader(shader);
-        canvas.drawBitmap(bitmap, new Matrix(), null);
-        canvas.drawCircle(x, y, 100, paint);
 
-        imageView.setImageBitmap(bmOverlay);
-        //imageView.buildDrawingCache();
+        canvas.drawBitmap(bitmap, new Matrix(), null);
+        canvas.drawCircle(x + POS_SIZE / 2, y, POS_SIZE, paint);
+
+        ImageView imageView = (ImageView) findViewById(R.id.map);
+        imageView.setImageDrawable(new BitmapDrawable(getResources(), bmOverlay));
         imageView.invalidate();
     }
 
