63a0d3fa4d9821e11d8fa218aa428a037b8fc887
jochy <alexandre.dubois-2@etu.univ-tours.fr>
2016-01-27 22:49:04 +0100

    Fix bdType

--- app/src/main/java/campus/m2dl/ane/campus/model/TagImg.java
+++ app/src/main/java/campus/m2dl/ane/campus/model/TagImg.java
@@ -6,26 +6,27 @@ import campus.m2dl.ane.campus.R;
  * Created by Alexandre on 21/01/2016.
  */
 public enum TagImg {
-    WATER("Eau", R.drawable.mpipe),
-    DEGRADATION("Dégradation", R.drawable.mrepaire),
-    RECYLCE("Recyclage", R.drawable.mtrash),
-    FOOD("Restauration", R.drawable.mfood),
-    BATTERY("Batterie", R.drawable.mbattery),
-    CARTBOARD("Carton", R.drawable.mcartboard),
-    GLASS("Verre", R.drawable.mglass),
-    TOILETS("Toilettes", R.drawable.mtoilets),
-    PAINT("Peinture", R.drawable.mpaint),
-    CONSTRUCTION("Travaux", R.drawable.mconstruction),
-    OTHER("Divers", R.drawable.munknow);
+    WATER("Eau", R.drawable.mpipe, "WATER"),
+    DEGRADATION("Dégradation", R.drawable.mrepaire, "DEGRADATION"),
+    RECYLCE("Recyclage", R.drawable.mtrash, "RECYCLE"),
+    FOOD("Restauration", R.drawable.mfood, "FOOD"),
+    BATTERY("Batterie", R.drawable.mbattery, "BATTERY"),
+    CARTBOARD("Carton", R.drawable.mcartboard, "CARTBOARD"),
+    GLASS("Verre", R.drawable.mglass, "GLASS"),
+    TOILETS("Toilettes", R.drawable.mtoilets, "TOILETS"),
+    PAINT("Peinture", R.drawable.mpaint, "PAINT"),
+    CONSTRUCTION("Travaux", R.drawable.mconstruction, "CONSTRUCTION"),
+    OTHER("Divers", R.drawable.munknow, "OTHER");
 
     public String text;
     public int resourceId;
+    public String bdType;
 
-    TagImg(String text, int resourceId) {
+    TagImg(String text, int resourceId, String dbType) {
         this.text = text;
         this.resourceId = resourceId;
+        this.bdType = dbType;
     }
-
 
     public static TagImg getValueOf(String s){
         for(TagImg t : values()){
@@ -40,5 +41,9 @@ public enum TagImg {
     @Override
     public String toString() {
         return this.text;
+    }
+
+    public String getBdType(){
+        return this.bdType;
     }
 }
